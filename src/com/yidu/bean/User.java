66aa848c7e5404e951eb61b22197d0c92a6ff456
YYing <2629504360@qq.com>
2020-12-24 15:09:35 +0800

    这是我的第一次提交！ 请妥善保管

---
+++ src/com/yidu/bean/User.java
@@ -0,0 +1,14 @@
+package com.yidu.bean;
+
+/**
+ * @author y99y
+ * @version 1.0
+ * @description:
+ * @date 2020/12/24 14:52
+ */
+public class User {
+
+    public static void main(String[] args) {
+        System.out.println("牛逼牛逼！");
+    }
+}
