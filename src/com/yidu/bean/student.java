8ca50b326270f104528c63b8e484c075137c1844
YYing <2629504360@qq.com>
2020-12-24 15:18:52 +0800

    第二次提交

---
+++ src/com/yidu/bean/student.java
@@ -0,0 +1,13 @@
+package com.yidu.bean;
+
+/**
+ * @author y99y
+ * @version 1.0
+ * @description:
+ * @date 2020/12/24 15:12
+ */
+public class student {
+    public static void main(String[] args) {
+        System.out.println("第二次");
+    }
+}
